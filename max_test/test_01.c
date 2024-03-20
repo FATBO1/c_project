4d9ac67a717907db4a1397d3ecf6557331b52098
FATBO1 <maxneo14@gmail.com>
2024-03-21 00:28:35 +0800

    added feature for users to choose file name when saving chart
    minor changes to save graph

--- max_test/test_01.c
+++ max_test/test_01.c
@@ -1,46 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_INPUT_LENGTH 10 // Maximum input length, excluding null terminator
 
 int main() {
-    int user_option;
+    char input[MAX_INPUT_LENGTH + 2]; // Buffer to hold user input (+2 for newline and null terminator)
 
-    while (1) {
-        // Print menu options
-        printf("1. Save chart to text file.\n");
-        printf("2. Modify chart.\n");
-        printf("3. New Chart.\n");
-        printf("4. Close Program.\n");
-        printf("Enter your option: ");
-        
-        // Take user input
-        scanf("%d", &user_option);
-        getchar();  // Clearing the input buffer
+    do {
+        printf("Enter a string (up to %d characters): ", MAX_INPUT_LENGTH);
+        fgets(input, sizeof(input), stdin); // Read user input
 
-        // Check user option
-        switch (user_option) {
-            case 1:
-                // Code to save chart to text file
-                printf("Saving chart to text file...\n");
-                // You can put your saving code here
-                break;
-            case 2:
-                // Code to modify chart
-                printf("Modifying chart...\n");
-                // You can put your modifying code here
-                break;
-            case 3:
-                // Code to create new chart
-                printf("Creating new chart...\n");
-                // You can put your code for creating new chart here
-                break;
-            case 4:
-                // Code to close the program
-                printf("Closing program...\n");
-                return 0; // Exit the program
-            default:
-                printf("Invalid option. Please enter a valid option.\n");
-                break;
+        // Remove newline character if present
+        input[strcspn(input, "\n")] = '\0';
+
+        // Check if input length is greater than MAX_INPUT_LENGTH
+        if (strlen(input) > MAX_INPUT_LENGTH) {
+            printf("Input exceeds maximum length! Please try again.\n");
         }
-    }
+    } while (strlen(input) > MAX_INPUT_LENGTH); // Loop while input length is greater than MAX_INPUT_LENGTH
+
+    printf("You entered: %s\n", input);
 
     return 0;
 }
