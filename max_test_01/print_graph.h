63d97d840efcd1e336d5bd01da01a4ac1588659f
FATBO1 <maxneo14@gmail.com>
2024-03-24 20:35:18 +0800

    chatgpt_api WIP

--- max_test_01/print_graph.h
+++ max_test_01/print_graph.h
@@ -5,15 +5,19 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <windows.h> // Include necessary header for Windows-specific functions
 
-#include "category.h"
+#include "category.h" // Include the header file for Category struct definition
+#include "coloured_bar.h" // Include the header file for coloured bar printing function
 
+
+// Function to print a horizontal bar chart
 void print_horizontal_bar_chart(char title[], Category categories[], int num_categories, char x_axis_label[])
 {
     int i, j;
-    float max_value = 0;
+    float max_value = 0; // Variable to store the maximum value among all categories
 
-    // Find the maximum value
+    // Find the maximum value among all categories
     for (i = 0; i < num_categories; i++)
     {
         if (categories[i].value > max_value)
@@ -22,33 +26,44 @@ void print_horizontal_bar_chart(char title[], Category categories[], int num_cat
         }
     }
 
-    // Scaling factor
+    // Determine the scaling factor to fit the bars within the display width
     float scaling_factor = max_value > 134 ? max_value / 134 : 1;
 
     // Print the chart
     printf("\n");
     printf("%*s\n\n", 75 + strlen(title) / 2, title);
 
+    // Get console information for handling colors using Windows Console API
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
+    GetConsoleScreenBufferInfo(hConsole, &consoleInfo);
+    WORD saved_attributes_first = consoleInfo.wAttributes;
+
+    // Define attributes for white text on black background
+    WORD whiteForegroundBlackBackground = FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY; // White text
+
     for (i = 0; i < num_categories; i++)
     {
-        printf("%15s|\n", " ");
+        printf("%15s|\n", " "); // Print space and vertical bar for alignment
         printf("%15s|", categories[i].name); // Print category name followed by a vertical bar
-        int bar_length = categories[i].value / scaling_factor;
-        for (j = 0; j < bar_length; ++j)
-        {
-            // printf("X");
-            //printf("%c", (char)254u);
-            printf("\033[0;34m%c", (char)254u);
-    
-            // Reset text color to default
-            printf("\033[0m");
-        }
-        printf("\n");
+        int bar_length = categories[i].value / scaling_factor; // Calculate the length of the bar based on the scaling factor
+        
+        // Print colored bar representing the category value
+        printColoredBar(bar_length, BACKGROUND_RED | BACKGROUND_INTENSITY);
+        
+        // Reset text color to white after printing the bar
+        SetConsoleTextAttribute(hConsole, whiteForegroundBlackBackground);
+
+        // Print the numerical value of the category
+        printf(" %.2f\n", categories[i].value);
     }
+
+    // Print separator line after all bars
     printf("%15s|\n", " ");
     printf("%15s+", " ");
 
-    // Print number of - before + to indicate tick marks
+    // Print tick marks to indicate scale on the X-axis
     for (int i = 0; i < 134; i++)
     {
         if (i == 49)
@@ -70,7 +85,7 @@ void print_horizontal_bar_chart(char title[], Category categories[], int num_cat
     }
     printf("\n");
 
-    // Print Tick Values
+    // Print tick values if scaling factor is applied
     if (scaling_factor != 1)
     {
         int tick1 = (max_value / 134) * 50;
@@ -79,19 +94,91 @@ void print_horizontal_bar_chart(char title[], Category categories[], int num_cat
         printf("%*d", 50, tick1);
         printf("%*d\n\n", 50, tick2);
     }
-    else
+    else // If no scaling factor, print default tick values
     {
         printf("%*s", 16, "0");
         printf("%*s", 50, "50");
         printf("%*s\n\n", 50, "100");
     }
 
-    // Print X-Axis Label
+    // Print X-axis label with appropriate padding for centering
     int label_padding = (150 - strlen(x_axis_label)) / 2;
     printf("%*s%s%*s\n\n", label_padding, "", x_axis_label, label_padding, "");
-
-    // Save Chart (Incomplete)
-    // printf("%s\n", "Do you want to save the chart? (yes/no): ");
 }
 
-#endif
+void print_vertical_bar_chart(char title[], Category categories[], int num_categories, char y_axis_label[])
+{
+    int i, j;
+    float max_value = 0;
+
+    // Find the maximum value among all categories
+    for (i = 0; i < num_categories; i++)
+    {
+        if (categories[i].value > max_value)
+        {
+            max_value = categories[i].value;
+        }
+    }
+
+    // Determine the scaling factor to fit the bars within the display height
+    float scaling_factor = max_value > 20 ? max_value / 20 : 1;
+
+    // Print the chart title
+    printf("\n%*s\n\n", 75 + strlen(title) / 2, title);
+
+    // Get console information for handling colors using Windows Console API
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
+    GetConsoleScreenBufferInfo(hConsole, &consoleInfo);
+    WORD saved_attributes_first = consoleInfo.wAttributes;
+
+    // Define attributes for white text on black background
+    WORD whiteForegroundBlackBackground = FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY;
+
+    // Print the vertical bars
+    for (i = 20; i >= 0; i--)
+    {
+        printf("%6.2f |", i * scaling_factor);
+
+        for (j = 0; j < num_categories; j++)
+        {
+            int bar_height = categories[j].value / scaling_factor;
+
+            if (bar_height >= i)
+            {
+                // Print colored bar segment
+                printColoredBar(1, BACKGROUND_RED | BACKGROUND_INTENSITY);
+            }
+            else
+            {
+                printf("   ");
+            }
+            printf("  ");
+        }
+        printf("\n");
+    }
+
+    // Reset text color to white
+    SetConsoleTextAttribute(hConsole, whiteForegroundBlackBackground);
+
+    // Print the category names below the bars
+    printf("       +");
+    for (i = 0; i < num_categories; i++)
+    {
+        printf("-----");
+    }
+    printf("\n        ");
+
+    for (i = 0; i < num_categories; i++)
+    {
+        printf("%-4s ", categories[i].name);
+    }
+    printf("\n\n");
+
+    // Print Y-axis label with appropriate padding for centering
+    int label_padding = (20 - strlen(y_axis_label)) / 2;
+    printf("%*s%s%*s\n\n", label_padding, "", y_axis_label, label_padding, "");
+}
+
+#endif // End of header guard
